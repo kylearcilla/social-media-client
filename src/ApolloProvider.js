e9b5e5546da3e7ff8992c74ab71a25a71ee4d580
Kyle Arcilla <kylearcilla@Kyles-MBP.fios-router.home>
2020-12-24 12:21:33 -0500

    edited proxy and server base URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -8,7 +8,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000'
+  uri: 'https://peaceful-beach-24392.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
